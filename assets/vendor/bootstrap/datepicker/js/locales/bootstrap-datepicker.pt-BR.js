fea32eeb40e2237dc6caf4bb22028289875015e9
fernando-fte <fernando.fte@gmail.com>
2014-02-24 16:38:33 -0300

    Adiciona pasta assets com vendors
    Arquivos de terceiros, incluindo:
    Bootstrap;
    Less;
    Coffe-Script;
    jQuery.

---
+++ assets/vendor/bootstrap/datepicker/js/locales/bootstrap-datepicker.pt-BR.js
@@ -0,0 +1,46 @@
+/**
+ * Brazilian translation for bootstrap-datepicker
+ * Cauan Cabral <cauan@radig.com.br>
+ */
+;(function($){
+	$.fn.datepicker.dates['pt-BR'] = {
+		days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"],
+		daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
+		daysMin: ["Do", "Se", "Te", "Qu", "Qu", "Se", "Sa", "Do"],
+		months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+		monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+		today: "Hoje",
+		clear: "Limpar"
+	};
+
+	$.fn.datepicker.dates['nav-pt-BR'] = {
+		days: [
+			"<span class=\"ltr-sx\">D</span><span class=\"ltr-sm\">Dom</span><span class=\"ltr-md\">Domingo</span>",
+			"<span class=\"ltr-sx\">S</span><span class=\"ltr-sm\">Seg</span><span class=\"ltr-md\">Segunda</span>",
+			"<span class=\"ltr-sx\">T</span><span class=\"ltr-sm\">Ter</span><span class=\"ltr-md\">Terça</span>",
+			"<span class=\"ltr-sx\">Q</span><span class=\"ltr-sm\">Qua</span><span class=\"ltr-md\">Quarta</span>",
+			"<span class=\"ltr-sx\">Q</span><span class=\"ltr-sm\">Qui</span><span class=\"ltr-md\">Quinta</span>",
+			"<span class=\"ltr-sx\">S</span><span class=\"ltr-sm\">Sex</span><span class=\"ltr-md\">Sexta</span>",
+			"<span class=\"ltr-sx\">S</span><span class=\"ltr-sm\">Sáb</span><span class=\"ltr-md\">Sábado</span>",
+			"<span class=\"ltr-sx\">D</span><span class=\"ltr-sm\">Dom</span><span class=\"ltr-md\">Domingo</span>"
+		],
+		months: [
+			"<span class=\"ltr-sx\">Jan</span> <span class=\"ltr-sm\">Jan</span> <span class=\"ltr-md\">Janeiro</span>",
+			"<span class=\"ltr-sx\">Fev</span> <span class=\"ltr-sm\">Fev</span> <span class=\"ltr-md\">Fevereiro</span>",
+			"<span class=\"ltr-sx\">Mar</span> <span class=\"ltr-sm\">Mar</span> <span class=\"ltr-md\">Março</span>",
+			"<span class=\"ltr-sx\">Abr</span> <span class=\"ltr-sm\">Abr</span> <span class=\"ltr-md\">Abril</span>",
+			"<span class=\"ltr-sx\">Mai</span> <span class=\"ltr-sm\">Mai</span> <span class=\"ltr-md\">Maio</span>",
+			"<span class=\"ltr-sx\">Jun</span> <span class=\"ltr-sm\">Jun</span> <span class=\"ltr-md\">Junho</span>",
+			"<span class=\"ltr-sx\">Jul</span> <span class=\"ltr-sm\">Jul</span> <span class=\"ltr-md\">Julho</span>",
+			"<span class=\"ltr-sx\">Ago</span> <span class=\"ltr-sm\">Ago</span> <span class=\"ltr-md\">Agosto</span>",
+			"<span class=\"ltr-sx\">Set</span> <span class=\"ltr-sm\">Set</span> <span class=\"ltr-md\">Setembro</span>",
+			"<span class=\"ltr-sx\">Out</span> <span class=\"ltr-sm\">Out</span> <span class=\"ltr-md\">Outubro</span>",
+			"<span class=\"ltr-sx\">Nov</span> <span class=\"ltr-sm\">Nov</span> <span class=\"ltr-md\">Novembro</span>",
+			"<span class=\"ltr-sx\">Dez</span> <span class=\"ltr-sm\">Dez</span> <span class=\"ltr-md\">Dezembro</span>"
+		],
+
+		monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+		today: "Hoje",
+		clear: "Limpar"
+	};
+}(jQuery));
